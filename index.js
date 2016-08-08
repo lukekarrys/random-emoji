8e436305f3b38e66dbad92e33623394327a797ae
Luke Karrys <lukekarrys@gmail.com>
2016-08-08 11:50:18 -0700

    deps

--- index.js
+++ index.js
@@ -1,67 +1,65 @@
-var emojis = require('emoji-named-characters');
-var shuffle = require('lodash.shuffle');
-var emojiNames = Object.keys(emojis);
+var emojis = require('emoji-named-characters')
+var shuffle = require('lodash.shuffle')
+var emojiNames = Object.keys(emojis)
 
-function imgSrc(host, name) {
-    if (!host) {
-        host = '/';
-    } else {
-        host = host.slice(-1) === '/' ? host : host + '/';
+function imgSrc (host, name) {
+  if (!host) {
+    host = '/'
+  } else {
+    host = host.slice(-1) === '/' ? host : host + '/'
+  }
+  return host + encodeURIComponent(name) + '.png'
+}
+
+function emojiImage (host, name, height) {
+  return '<img class="emoji" title="' + name + '" alt="' + name + '" src="' + imgSrc(host, name) + '"' + ' height="' + (height || 64) + '"' + ' />'
+}
+
+function mapEmoji (options) {
+  return function (emoji) {
+    return {
+      character: emojis[emoji].character,
+      name: emoji,
+      image: emojiImage(options.host, emoji, options.height),
+      imageSrc: imgSrc(options.host, emoji)
     }
-    return host + encodeURIComponent(name) + '.png';
+  }
 }
-
-function emojiImage(host, name, height) {
-    return '<img class="emoji" title="' + name + '" alt="' + name + '" src="' + imgSrc(host, name) + '"' + ' height="' + (height || 64) + '"' + ' />';
-}
-
-function mapEmoji(options) {
-    return function (emoji) {
-        return {
-            character: emojis[emoji].character,
-            name: emoji,
-            image: emojiImage(options.host, emoji, options.height),
-            imageSrc: imgSrc(options.host, emoji)
-        };
-    };
-}
-
 
 exports.random = function (options) {
-    if (!options) {
-        options = {};
-    }
-    return shuffle(emojiNames.slice(0)).slice(0, options.count || 3).map(mapEmoji(options));
-};
+  if (!options) {
+    options = {}
+  }
+  return shuffle(emojiNames.slice(0)).slice(0, options.count || 3).map(mapEmoji(options))
+}
 
+function fetchSyllables (count, randomEmoji) {
+  var result = []
+  var current = 0
+  var currentEmoji
+  var emojiCount
 
-function fetchSyllables(count, randomEmoji) {
-    var result = [];
-    var current = 0;
-    var currentEmoji;
-    var emojiCount;
+  while (current < count) {
+    currentEmoji = randomEmoji.pop()
+    emojiCount = emojis[currentEmoji].syllables
 
-    while (current < count) {
-        currentEmoji = randomEmoji.pop();
-        emojiCount = emojis[currentEmoji].syllables;
-
-        if (emojiCount === 0 || (current + emojiCount) > count) {
-            continue;
-        }
-
-        result.push(currentEmoji);
-        current += emojiCount;
+    if (emojiCount === 0 || (current + emojiCount) > count) {
+      continue
     }
 
-    return result;
+    result.push(currentEmoji)
+    current += emojiCount
+  }
+
+  return result
 }
 
 exports.haiku = function (options) {
-    var asEmoji = mapEmoji(options || {});
-    var randomEmoji = shuffle(emojiNames.slice(0));
-    return [
-        fetchSyllables(5, randomEmoji).map(asEmoji),
-        fetchSyllables(7, randomEmoji).map(asEmoji),
-        fetchSyllables(5, randomEmoji).map(asEmoji)
-    ];
-};
+  var asEmoji = mapEmoji(options || {})
+  var randomEmoji = shuffle(emojiNames.slice(0))
+  return [
+    fetchSyllables(5, randomEmoji).map(asEmoji),
+    fetchSyllables(7, randomEmoji).map(asEmoji),
+    fetchSyllables(5, randomEmoji).map(asEmoji)
+  ]
+}
--- test/test.js
+++ test/test.js
@@ -1,65 +1,64 @@
-var Lab = require('lab');
-var Code = require('code');
-var randomEmoji = require('../');
-var lab = exports.lab = Lab.script();
+var Lab = require('lab')
+var Code = require('code')
+var randomEmoji = require('../')
+var lab = exports.lab = Lab.script()
 
 lab.experiment('Random', function () {
-    lab.test('Returns some emoji without options', function (done) {
-        var emoji = randomEmoji.random();
-        Code.expect(Array.isArray(emoji)).to.equal(true);
-        Code.expect(emoji.length).to.equal(3);
-        Code.expect(typeof emoji[0].name).to.equal('string');
-        done();
-    });
+  lab.test('Returns some emoji without options', function (done) {
+    var emoji = randomEmoji.random()
+    Code.expect(Array.isArray(emoji)).to.equal(true)
+    Code.expect(emoji.length).to.equal(3)
+    Code.expect(typeof emoji[0].name).to.equal('string')
+    done()
+  })
 
-    lab.test('Returns some emoji with options', function (done) {
-        var emoji = randomEmoji.random({
-            count: 5,
-            host: '/images/'
-        });
-        Code.expect(emoji.length).to.equal(5);
-        Code.expect(emoji[0].imageSrc.indexOf('/images/')).to.equal(0);
-        done();
-    });
+  lab.test('Returns some emoji with options', function (done) {
+    var emoji = randomEmoji.random({
+      count: 5,
+      host: '/images/'
+    })
+    Code.expect(emoji.length).to.equal(5)
+    Code.expect(emoji[0].imageSrc.indexOf('/images/')).to.equal(0)
+    done()
+  })
 
-    lab.test('Returns some emoji with host name with trailing slash', function (done) {
-        var emoji = randomEmoji.random({
-            count: 5,
-            host: '/images'
-        });
-        Code.expect(emoji.length).to.equal(5);
-        Code.expect(emoji[0].imageSrc.indexOf('/images/')).to.equal(0);
-        done();
-    });
+  lab.test('Returns some emoji with host name with trailing slash', function (done) {
+    var emoji = randomEmoji.random({
+      count: 5,
+      host: '/images'
+    })
+    Code.expect(emoji.length).to.equal(5)
+    Code.expect(emoji[0].imageSrc.indexOf('/images/')).to.equal(0)
+    done()
+  })
 
-    lab.test('Returns some emoji with height', function (done) {
-        var emoji = randomEmoji.random({
-            height: '100'
-        });
-        Code.expect(emoji[0].image.indexOf('height="100"')).to.not.equal(-1);
-        done();
-    });
-});
-
+  lab.test('Returns some emoji with height', function (done) {
+    var emoji = randomEmoji.random({
+      height: '100'
+    })
+    Code.expect(emoji[0].image.indexOf('height="100"')).to.not.equal(-1)
+    done()
+  })
+})
 
 lab.experiment('Haikus', function () {
-    lab.test('Returns a haiku without options', function (done) {
-        var emoji = randomEmoji.haiku();
-        Code.expect(Array.isArray(emoji)).to.equal(true);
-        Code.expect(emoji.length).to.equal(3);
-        Code.expect(Array.isArray(emoji[0])).to.equal(true);
-        Code.expect(Array.isArray(emoji[1])).to.equal(true);
-        Code.expect(Array.isArray(emoji[2])).to.equal(true);
-        done();
-    });
+  lab.test('Returns a haiku without options', function (done) {
+    var emoji = randomEmoji.haiku()
+    Code.expect(Array.isArray(emoji)).to.equal(true)
+    Code.expect(emoji.length).to.equal(3)
+    Code.expect(Array.isArray(emoji[0])).to.equal(true)
+    Code.expect(Array.isArray(emoji[1])).to.equal(true)
+    Code.expect(Array.isArray(emoji[2])).to.equal(true)
+    done()
+  })
 
-    lab.test('Returns a haiku with options', function (done) {
-        var emoji = randomEmoji.haiku({
-            host: '/images/',
-            height: 75
-        });
-        Code.expect(emoji[0][0].image.indexOf('height="75"')).to.not.equal(-1);
-        Code.expect(emoji[0][0].imageSrc.indexOf('/images/')).to.equal(0);
-        done();
-    });
-});
+  lab.test('Returns a haiku with options', function (done) {
+    var emoji = randomEmoji.haiku({
+      host: '/images/',
+      height: 75
+    })
+    Code.expect(emoji[0][0].image.indexOf('height="75"')).to.not.equal(-1)
+    Code.expect(emoji[0][0].imageSrc.indexOf('/images/')).to.equal(0)
+    done()
+  })
+})
